1067c8f2d2e6b6af244774c3a565e9be39b3e178
Foxgv <Foxgv@google.com>
2022-11-19 18:05:28 +0200

    Step 8

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,23 +1,29 @@
 package com.game.entity;
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 
-
+@Entity
+@Table(name = "player", schema = "rpg")
 public class Player {
+    @Id
     private Long id;
-
+    @Column(name = "name", nullable = false, length = 12)
     private String name;
-
+    @Column(name = "title", nullable = false, length = 30)
     private String title;
-
+    @Column(name = "race", nullable = false)
+    @Enumerated(EnumType.ORDINAL)
     private Race race;
-
+    @Column(name = "profession", nullable = false)
+    @Enumerated(EnumType.ORDINAL)
     private Profession profession;
-
+    @Column(name = "birthday", nullable = false)
     private Date birthday;
-
+    @Column(name = "banned", nullable = false)
     private Boolean banned;
-
+    @Column(name = "level", nullable = false)
     private Integer level;
 
     public Player() {
